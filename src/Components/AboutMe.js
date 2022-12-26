16483a4384a3ffcd78c6d3ac33ef4a2e906a8404
Nedim Begic <nedim.begic93@gmail.com>
2022-12-26 23:42:55 +0100

    adding bug gif

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -4,6 +4,10 @@ import style from './AboutMe.module.css';
 import aboutMe from '../files/Aboutme.pdf';
 import resume from '../files/resume.pdf';
 import postman from '../images/postman.png';
+import codewars from '../images/codewars.png';
+import github from '../images/github.png';
+import codepen from '../images/codepen.png';
+import portfolio from '../images/portfolio.png';
 
 const AboutMe = props => {
 
@@ -12,25 +16,37 @@ const AboutMe = props => {
             <img className={style.pic} src={profile} alt="" />
             <h5 >Nedim Begić</h5>
             <div className={style.info}>
-                <a href={aboutMe} target="_blank">About Me </a>
-                <a href={resume} target='_blank'>Resume</a>
+                <a className={style.files} href={aboutMe} target="_blank">About Me </a>
+                <a className={style.files} href={resume} target='_blank'>Resume</a>
             </div>
             <h4>Tools I use:</h4>
             <div className={style.icons}>
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" />
-            <img src={postman} alt=""/>
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" />
+                <img src={postman} alt="" />
             </div>
             <h4>Basic knowledge:</h4>
             <div className={style.icons}>
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
-            <i className="devicon-express-original-wordmark"></i>
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
+                <i className="devicon-express-original-wordmark"></i>
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" />
             </div>
-            
+            <div className={style.coding}>
+                <h4>Exampe of code:</h4>
+                <div className={style.code}>
+                    <div>
+                        <a href='https://github.com/NedimBegic' target='_blank'><img src={github} alt="github" /></a>
+                        <a href="https://www.codewars.com/users/NedimBuBa/completed_solutions" target='_blank'><img src={codewars} alt="codewars" /></a>
+                    </div>
+                    <div>
+                        <a href="https://codepen.io/your-work/" target='_blank'> <img src={codepen} alt="codepen" /></a>
+                        <a href="#" target='_blank'><img src={portfolio} alt="portfolio"/></a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Connect.js
+++ src/Components/Connect.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import styles from './Connect.module.css';
+import gif from '../images/bug.gif';
 
 const Connect = props => {
 
     return (
-        <div id='connect' className={styles.box}>
-            <h1>Connect</h1>
-            <div>
-            <a href="#" className={styles.fb + " " + "fa fa-facebook" + " " + styles.icon}></a>
-            <a href="#" className={styles.ln + " " + "fa fa-linkedin" + " " + styles.icon}></a>
-            <a href="#" className={styles.in + " " + "fa fa-instagram" + " " + styles.icon}></a>
-            <a href="#" className={styles.gh + " " + "fa fa-github" + " " + styles.icon}></a>
+        <div className={styles.all}>
+            <div id='connect' className={styles.box}>
+                <h1>Connect</h1>
+                <div>
+                    <a href="https://github.com/NedimBegic" target='_blank' className={styles.gh + " " + "fa fa-github" + " " + styles.icon}></a>
+                    <a href="https://www.linkedin.com/in/nedim-begic-a42210245/" target='_blank' className={styles.ln + " " + "fa fa-linkedin" + " " + styles.icon}></a>
+                    <a href="https://www.facebook.com/nedim.b.begic/" target='_blank' className={styles.fb + " " + "fa fa-facebook" + " " + styles.icon}></a>
+                    <a href="https://www.instagram.com/nedim_begic93/" target='_blank' className={styles.in + " " + "fa fa-instagram" + " " + styles.icon}></a>
+                </div>
+                <div>
+                    <i> 📱 - 064/4078534</i>
+                </div>
             </div>
-            <div>
-            <i> 📱 - 064/4078534</i>
-            </div>
-        </div>
+            <img className={styles.bug} src={gif} alt="bug" />
+            {/*             <div className={styles.road}></div>
+ */}        </div>
     );
 };
 
